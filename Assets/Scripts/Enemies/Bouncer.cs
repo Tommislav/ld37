84d8587aacc8b5863291f8b684664afd39ce75f9
Tommislav <tommy@salomonsson.se>
2016-12-11 02:55:45 +0100

    lots of graphics now

---
+++ Assets/Scripts/Enemies/Bouncer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouncer : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float f = 100f;
+
+	float vx;
+	float vy;
+
+	private int cnt;
+
+	void Start () {
+		rb = Find.ComponentOnGameObject<Rigidbody2D>(this);
+		vx = Random.value < 0.5f ? -1f : 1f;
+		vy = Random.value < 0.5f ? -1f : 1f;
+		rb.AddForce((new Vector2(vx, vy) * f), ForceMode2D.Impulse);
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.collider.gameObject.layer != LayerMask.NameToLayer("World")) {
+			return;
+		}
+
+		Vector2 cPos = coll.contacts[0].point;
+		Vector2 me = new Vector2(transform.position.x, transform.position.y);
+		Vector2 rel = cPos - me;
+
+		if (Mathf.Abs(rel.x) >= Mathf.Abs(rel.y)) {
+			vx = -vx;
+		}
+		if (Mathf.Abs(rel.x) <= Mathf.Abs(rel.y)) {
+			vy = -vy;
+		}
+
+		rb.velocity = Vector2.zero;
+		rb.AddForce((new Vector2(vx, vy) * f), ForceMode2D.Impulse);
+	}
+
+	void Update() {
+		if (--cnt <= 0) {
+			cnt = 10;
+
+			if (rb.velocity.magnitude > 2f) {
+				rb.velocity = rb.velocity.normalized * 2f;
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,11 +7,20 @@ public class PlayerControls : MonoBehaviour {
 
     private Rigidbody2D rb;
 	private float moveForce = 200f;
-	private bool isTransition;
+
+	private GameObject swordU;
+	private GameObject swordD;
+	private GameObject swordL;
+	private GameObject swordR;
+
+	private GameObject[] swords;
+	private bool isAttacking;
 
     void Awake() {
 		Instance = this;
-        rb = GetComponent<Rigidbody2D>();
+		rb = Find.ComponentOnGameObject<Rigidbody2D>(this);
+		swords = new GameObject[] { Find.ChildByName(this, "SwordU"), Find.ChildByName(this, "SwordD"), Find.ChildByName(this, "SwordL"), Find.ChildByName(this, "SwordR") };
+		HideAndGet(-1);
     }
 	void OnDestroy() {
 		Instance = null;
@@ -47,5 +56,32 @@ public class PlayerControls : MonoBehaviour {
 		}
 
 		rb.drag = move ? 5 : 15;
+
+		int atk = -1;
+		atk = Input.GetKey(KeyCode.W) ? 0:atk;
+		atk = Input.GetKey(KeyCode.S) ? 1 : atk;
+		atk = Input.GetKey(KeyCode.A) ? 2 : atk;
+		atk = Input.GetKey(KeyCode.D) ? 3 : atk;
+	
+		if (atk != -1 && !isAttacking) {
+			LeanTween.cancel(gameObject);
+			isAttacking = true;
+			GameObject sword = HideAndGet(atk);
+			sword.SetActive(true);
+			LeanTween.delayedCall(gameObject, 0.18f, () => sword.SetActive(false));
+		}
+		if (atk == -1) {
+			isAttacking = false;
+		}
+	}
+
+	private GameObject HideAndGet(int dir) {
+		for (int i=0; i<swords.Length; i++) {
+			swords[i].SetActive(false);
+		}
+		if (dir >= 0 && dir < swords.Length) {
+			return swords[dir];
+		}
+		return null;
 	}
 }
